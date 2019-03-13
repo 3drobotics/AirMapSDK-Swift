549efc753dddd6494514be37a2299920f93d9bc2
Dmitry Malakhov <dmitry.malakhov@3drobotics.com>
2019-03-12 19:08:32 -0700

    Fix for Travis

---
+++ bridgeHeaders.h
@@ -0,0 +1,15 @@
+//
+//  bridgeHeaders.h
+//  AirMap
+//
+//  Created by Dmitry Malakhov on 1/14/19.
+//  Copyright © 2019 AirMap, Inc. All rights reserved.
+//
+
+#import "CLLocation+Utils.h"
+
+#ifndef bridgeHeaders_h
+#define bridgeHeaders_h
+
+
+#endif /* bridgeHeaders_h */
